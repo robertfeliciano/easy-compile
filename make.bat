b361f88969f8cda698659a2d7f787420390bb935
Robert Feliciano <67375744+robertfeliciano@users.noreply.github.com>
2021-12-27 13:26:10 -0500

    added make.bat
    this is it! hopefully the final version

---
+++ make.bat
@@ -0,0 +1,29 @@
+@echo off
+
+for %%f in (.\*) do (
+    if "%1"=="%%~nf" (
+        if "%%~xf"==".c" (
+            set file="%%f"
+            set name="%%~nf"
+            goto compile_c
+        )
+    )
+    if "%1"=="%%~nf" (
+        if "%%~xf"==".cpp" (
+            set file="%%f"
+            set name="%%~nf"
+            goto compile_cpp
+        )
+    )
+)
+echo Error: No project with that name found in this directory.
+
+:compile_c 
+gcc %file% -o %name%
+echo Project compiled successfully.
+goto :eof
+
+:compile_cpp
+g++ %file% -o %name%
+echo Project compiled successfully.
+goto :eof
